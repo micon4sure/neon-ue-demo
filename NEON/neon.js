7ec9f21434ee7b436ddff253a86d22b0c447f4e0
mICON <micon@techtile.media>
2025-01-15 14:05:09 +0100

    Updated to latest neon-ue-web

--- NEON/neon.js
+++ NEON/neon.js
@@ -1,51 +1,48 @@
 // index.ts
 var NEON;
 (function(NEON) {
-  let loggingEnabled = true;
-  function setLoggingEnabled(enabled) {
-    console.log("NEON logging set to ", enabled ? "enabled" : "disabled");
-    loggingEnabled = enabled;
-  }
-  NEON.setLoggingEnabled = setLoggingEnabled;
-  function log(...args) {
-    if (loggingEnabled) {
-      console.log(...args);
-    }
-  }
-  NEON.log = log;
-  function error(...args) {
-    if (loggingEnabled) {
-      console.error(...args);
-    }
-  }
-  NEON.error = error;
-  function InvokeUnrealEvent(delegate, data = {}) {
+  function invokeUnrealEvent(delegate, data = {}) {
     return NEON_Bridge_Unreal.invokeUnrealEvent(delegate, data);
   }
-  NEON.InvokeUnrealEvent = InvokeUnrealEvent;
-  NEON.InvokeUnreal = InvokeUnrealEvent;
-  function InvokeUnrealFunction(delegate, data = {}) {
+  NEON.invokeUnrealEvent = invokeUnrealEvent;
+  NEON.invokeUnreal = invokeUnrealEvent;
+  function invokeUnrealFunction(delegate, data = {}) {
     return NEON_Bridge_Unreal.invokeUnrealFunction(delegate, data);
   }
-  NEON.InvokeUnrealFunction = InvokeUnrealFunction;
-  function OnInvoke(delegate, callback) {
+  NEON.invokeUnrealFunction = invokeUnrealFunction;
+  function onInvoke(delegate, callback) {
     NEON_Bridge_Web.registerCallback(delegate, callback);
   }
-  NEON.OnInvoke = OnInvoke;
-  NEON.OnInvokeWeb = OnInvoke;
-  function InvokeWeb(delegate, data) {
+  NEON.onInvoke = onInvoke;
+  function invoke(delegate, data) {
     NEON_Bridge_Web.invoke(delegate, data);
   }
-  NEON.InvokeWeb = InvokeWeb;
+  NEON.invoke = invoke;
+  function setVerbose(verbose) {
+    Log.setVerbose(verbose);
+  }
+  NEON.setVerbose = setVerbose;
 })(NEON || (NEON = {}));
-window.NEON_Bridge_Web_Invoke = (method, data) => {
-  NEON_Bridge_Web.invoke(method, data);
-};
+
+class Log {
+  static verbose = false;
+  static setVerbose(verbose) {
+    Log.verbose = verbose;
+  }
+  static info(...args) {
+    if (!Log.verbose)
+      return;
+    console.log(`[NEON]`, ...args);
+  }
+  static error(...args) {
+    console.error(`[NEON]`, ...args);
+  }
+}
 
 class NEON_Bridge_Web {
   static callbacks = {};
   static registerCallback(id, callback) {
-    NEON.log("Registering NEON callback", id);
+    Log.info("Registering NEON callback", id);
     NEON_Bridge_Web.callbacks[id] = callback;
   }
   static invoke(id, dataRaw = "{}") {
@@ -53,14 +50,14 @@ class NEON_Bridge_Web {
     try {
       data = JSON.parse(dataRaw);
     } catch (e) {
-      NEON.error("Invoke NEON web callback failed: data is not JSON parseable: ", data);
+      Log.error("Invoke NEON web callback failed: data is not JSON parseable: ", dataRaw);
       return;
     }
     if (!NEON_Bridge_Web.callbacks[id]) {
-      NEON.error(`Invoke NEON web callback failed: callback not found: ${id}`);
+      Log.error(`Invoke NEON web callback failed: callback not found: ${id}`);
       return;
     }
-    NEON.log("Invoke NEON web callback", id, data);
+    Log.info("Invoke NEON web callback", id, data);
     NEON_Bridge_Web.callbacks[id](data);
   }
 }
@@ -71,14 +68,14 @@ class NEON_Bridge_Unreal {
   }
   static invokeUnrealFunction(delegate, data) {
     if (!delegate) {
-      NEON.error("NEON.invokeUnrealFunction failed: delegate is required");
+      Log.error("NEON.invokeUnrealFunction failed: delegate is required");
       return Promise.reject({ errorCode: 101, errorMessage: "Delegate is required" });
     }
     delegate = "Invoke_" + delegate;
-    NEON.log("NEON.invokeUnrealFunction", delegate, data);
+    Log.info("NEON.invokeUnrealFunction", delegate, data);
     return new Promise((resolve, reject) => {
       if (!window.cefQuery) {
-        NEON.error("NEON.invokeUnrealFunction failed: cefQuery is not defined");
+        Log.error("NEON.invokeUnrealFunction failed: cefQuery is not defined");
         return reject({ errorCode: 103, errorMessage: "cefQuery is not defined" });
       }
       window.cefQuery({
@@ -88,17 +85,17 @@ class NEON_Bridge_Unreal {
           parameters: data
         }),
         onSuccess: function(response) {
-          NEON.log(`NEON.invokeUnrealFunction[${delegate}] succeeded: ${response}`);
+          Log.info(`NEON.invokeUnrealFunction[${delegate}] succeeded: ${response}`);
           try {
             const result = JSON.parse(response);
             resolve(result);
           } catch (e) {
-            NEON.error(`NEON.invokeUnrealFunction[${delegate}] failed to parse response: ${response}`);
+            Log.error(`NEON.invokeUnrealFunction[${delegate}] failed to parse response: ${response}`);
             reject({ errorCode: 102, errorMessage: "Failed to parse response" });
           }
         },
         onFailure: function(errorCode, errorMessage) {
-          NEON.error(`NEON.invokeUnrealFunction[${delegate}] failed: ${errorCode} - ${errorMessage}`);
+          Log.error(`NEON.invokeUnrealFunction[${delegate}] failed: ${errorCode} - ${errorMessage}`);
           reject({ errorCode, errorMessage });
         }
       });
@@ -106,14 +103,14 @@ class NEON_Bridge_Unreal {
   }
   static invokeUnrealEvent(delegate, data = {}) {
     if (!delegate) {
-      NEON.error("NEON.invokeUnrealFunction failed: delegate is required");
+      Log.error("NEON.invokeUnrealFunction failed: delegate is required");
       return Promise.reject({ errorCode: 101, errorMessage: "Delegate is required" });
     }
     delegate = "OnInvoke_" + delegate;
-    NEON.log("NEON.invokeUnrealEvent", delegate, data);
+    Log.info("NEON.invokeUnrealEvent", delegate, data);
     return new Promise((resolve, reject) => {
       if (!window.cefQuery) {
-        NEON.error("NEON.invokeUnrealFunction failed: cefQuery is not defined");
+        Log.error("NEON.invokeUnrealFunction failed: cefQuery is not defined");
         return reject({ errorCode: 103, errorMessage: "cefQuery is not defined" });
       }
       window.cefQuery({
@@ -123,18 +120,20 @@ class NEON_Bridge_Unreal {
           parameters: data
         }),
         onSuccess: function(response) {
-          NEON.log(`NEON.invokeUnrealEvent[${delegate}] succeeded.`);
+          Log.info(`NEON.invokeUnrealEvent[${delegate}] succeeded.`);
           resolve(null);
         },
         onFailure: function(errorCode, errorMessage) {
-          NEON.error(`NEON.invokeUnrealEvent failed: ${errorCode} - ${errorMessage}`);
+          Log.error(`NEON.invokeUnrealEvent[${delegate}] failed: ${errorCode} - ${errorMessage}`);
           reject({ errorCode, errorMessage });
         }
       });
     });
   }
 }
-var NEON_web_default = NEON;
+window.NEON_Bridge_Web_Invoke = NEON.invoke;
+var neon_ue_web_default = NEON;
 export {
-  NEON_web_default as default
+  neon_ue_web_default as default,
+  NEON_Bridge_Web
 };
